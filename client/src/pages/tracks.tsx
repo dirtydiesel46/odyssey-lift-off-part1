f160ab1c19957998ac27134b518e99fb652a4d2a
danieldupreez <daniel@trooptravel.com>
2023-12-19 12:06:49 +0400

    Refactor Tracks page to use QueryResult component

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -3,6 +3,8 @@ import { Layout } from "../components";
 import { gql } from "../__generated__";
 import { useQuery } from "@apollo/client";
 import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
+
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
@@ -26,13 +28,13 @@ query GetTracks {
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
   return (
     <Layout grid>
-      {data?.tracksForHome?.map((track) => (
-        <TrackCard key={track.id} track={track} />
-      ))}
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
     </Layout>
   );
 };
