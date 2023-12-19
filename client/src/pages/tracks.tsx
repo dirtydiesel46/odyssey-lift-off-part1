25e880ac368ea74ca82ef36b5846b946d70c77cc
danieldupreez <daniel@trooptravel.com>
2023-12-19 11:47:57 +0400

    Refactor TrackCard component and add TrackCard to Tracks page

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Layout } from "../components";
 import { gql } from "../__generated__";
 import { useQuery } from "@apollo/client";
-
+import TrackCard from "../containers/track-card";
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
@@ -28,6 +28,12 @@ const Tracks = () => {
 
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-  return <Layout grid>{JSON.stringify(data)}</Layout>;
+  return (
+    <Layout grid>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </Layout>
+  );
 };
 export default Tracks;
