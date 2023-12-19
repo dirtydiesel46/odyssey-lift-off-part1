ae8676469c0cdf4b2b20d0f0fc8087a2e8f69ab1
danieldupreez <daniel@trooptravel.com>
2023-12-19 11:29:45 +0400

    Refactor index.tsx and add TRACKS query to tracks.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import GlobalStyles from "./styles";
 import Pages from "./pages";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
 const client = new ApolloClient({
   // options
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
-})
+});
 ReactDOM.render(
   <ApolloProvider client={client}>
     <GlobalStyles />
--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
-const Tracks = () => {
-  return <Layout grid> </Layout>;
-};
+const TRACKS = gql(`
+query GetTracks {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}`);
 
-export default Tracks;
+
+export default TRACKS;
