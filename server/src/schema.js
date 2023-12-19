612e21166d9fb26ea856b07185e5c2e188ecb8f5
danieldupreez <daniel@trooptravel.com>
2023-12-19 09:48:57 +0400

    Add Query and Track types to schema.js

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,5 +1,32 @@
 import gql from "graphql-tag";
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    "Query to get all tracks"
+    tracksForHome: [Track!]!
+  }
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "the track's title"
+    title: String!
+    "the track's main author"
+    author: Author!
+    "the track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "the tracks approximate length to complete, in minutes"
+    length: Int
+    "the number of modules this track contains"
+    modulesCount: Int
+  }
 
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    "the author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+`;
 module.exports = typeDefs;
